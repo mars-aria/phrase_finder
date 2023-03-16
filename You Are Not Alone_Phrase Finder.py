b3ce25d1b1f2f0f802de6e249422ad6f0b9d6880
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-03-16 15:17:15 -0500

    Update You Are Not Alone_Phrase Finder.py

--- You Are Not Alone_Phrase Finder.py
+++ You Are Not Alone_Phrase Finder.py
@@ -9,45 +9,32 @@
 
 # In[1036]:
 
-
 source = open('408-0.txt', encoding= 'utf-8')
 souls = source.read()
 source.close()
 print(souls[0:1000])
 
-
 # In[1037]:
-
 
 import re
 
-
 # In[1301]:
-
 
 souls_dict = re.findall(r'black woman', souls)
 
-
 # In[1302]:
-
 
 souls_dict
 
-
 # In[1303]:
-
 
 len(souls_dict)
 
-
 # In[1304]:
-
 
 phrase = ''.join(souls)
 
-
 # In[1305]:
-
 
 def phrase_finder(phrase):
     if phrase in souls_dict:
@@ -56,60 +43,42 @@ def phrase_finder(phrase):
     elif phrase not in souls_dict:
         return 'Phrase not found, but you are not alone. 🤗'
 
-
 # In[1306]:
-
 
 phrase_finder('black woman')
 
-
 # In[1307]:
-
 
 phrase_finder('weary soul')
 
-
 # In[1259]:
-
 
 source = open('1260-0.txt', encoding= 'utf-8')
 jane = source.read()
 source.close()
 print(jane[0:1000])
 
-
 # In[1260]:
-
 
 import re
 
-
 # In[1378]:
-
 
 jane_dict = re.findall(r'frantic bird', jane)
 
-
 # In[1379]:
-
 
 len(jane_dict)
 
-
 # In[1370]:
-
 
 jane_dict
 
-
 # In[1371]:
-
 
 phrase = ''.join(jane)
 
-
 # In[1372]:
-
 
 def phrase_finder(phrase):
     if phrase in jane_dict:
@@ -118,66 +87,42 @@ def phrase_finder(phrase):
     elif phrase not in jane_dict:
         return 'Phrase not found, but you are not alone. 🤗'
 
-
 # In[1375]:
-
 
 phrase_finder('frantic bird')
 
-
 # In[1380]:
-
 
 phrase_finder('rare jewel')
 
-
-# In[ ]:
-
-
-
-
-
 # In[1381]:
-
 
 source = open('43-0.txt', encoding= 'utf-8')
 jekyll = source.read()
 source.close()
 print(jekyll[0:1000])
 
-
 # In[1382]:
-
 
 import re
 
-
 # In[1396]:
-
 
 jekyll_dict = re.findall(r'beloved daydream', jekyll)
 
-
 # In[1397]:
-
 
 len(jekyll_dict)
 
-
 # In[1398]:
-
 
 jekyll_dict
 
-
 # In[1399]:
-
 
 phrase = ''.join(jekyll)
 
-
 # In[1400]:
-
 
 def phrase_finder(phrase):
     if phrase in jekyll_dict:
@@ -186,54 +131,38 @@ def phrase_finder(phrase):
     elif phrase not in frankenstein_dict:
         return 'Phrase not found, but you are not alone. 🤗'
 
-
 # In[1401]:
-
 
 phrase_finder('beloved daydream')
 
-
 # In[1402]:
-
 
 phrase_finder('weirdo')
 
-
 # In[1403]:
-
 
 source = open('84-0.txt', encoding= 'utf-8')
 frankenstein = source.read()
 source.close()
 print(frankenstein[0:1000])
 
-
 # In[1404]:
-
 
 import re
 
-
 # In[1426]:
-
 
 frankenstein_dict = re.findall(r'fallen angel', frankenstein)
 
-
 # In[1427]:
-
 
 len(frankenstein_dict)
 
-
 # In[1428]:
-
 
 phrase = ''.join(frankenstein)
 
-
 # In[1429]:
-
 
 def phrase_finder(phrase):
     if phrase in frankenstein_dict:
@@ -242,21 +171,10 @@ def phrase_finder(phrase):
     elif phrase not in frankenstein_dict:
         return 'Phrase not found, but you are not alone. 🤗'
 
-
 # In[1430]:
-
 
 phrase_finder('fallen angel')
 
-
 # In[1432]:
 
-
 phrase_finder('secret wonder')
-
-
-# In[ ]:
-
-
-
-
